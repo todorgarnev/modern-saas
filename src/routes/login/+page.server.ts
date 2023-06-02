2904342284533d82829adb5147b10de1c177da02
Todor Garnev <tgarnev@abv.bg>
2023-06-02 16:42:21 +0300

    Added login functionality

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,43 @@
+import { fail, redirect } from "@sveltejs/kit";
+import { AuthApiError } from "@supabase/supabase-js";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import { z } from "zod";
+import type { Actions, PageServerLoad } from "./$types";
+
+const loginUserSchema = z.object({
+	email: z.string().email("Please enter a valid email address"),
+	password: z.string().min(1, "Please enter a password")
+});
+
+export const load: PageServerLoad = async () => {
+	return {
+		form: superValidate(loginUserSchema)
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const form = await superValidate(event, loginUserSchema);
+
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		const { error: authError } = await event.locals.supabase.auth.signInWithPassword(form.data);
+
+		if (authError) {
+			if (authError instanceof AuthApiError && authError.status === 400) {
+				setError(form, "email", "Invalid credentials");
+				setError(form, "password", "Invalid credentials");
+
+				return fail(400, {
+					form
+				});
+			}
+		}
+
+		throw redirect(302, "/");
+	}
+};
