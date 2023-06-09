27aad0b06a1df21d107945f902d09fe3789a6162
Todor Garnev <tgarnev@abv.bg>
2023-06-09 18:10:51 +0300

    Fixed multiple plans at the same time issue

--- src/routes/api/stripe/checkout/+server.ts
+++ src/routes/api/stripe/checkout/+server.ts
@@ -1,15 +1,18 @@
 import { error, redirect } from "@sveltejs/kit";
-import { getCustomerRecord } from "$lib/server/customers";
-import { stripe } from "$lib/server/stripe";
-import { ENV } from "$lib/server/env";
+import { createCheckoutSession, getSubscriptionTier } from "$lib/server/subscriptions";
 import type { RequestHandler } from "./$types";
-import { createCheckoutSession } from "$lib/server/subscriptions";
 
 export const GET: RequestHandler = async (event) => {
 	const session = await event.locals.getSession();
 
 	if (!session) {
 		throw redirect(302, "/login");
+	}
+
+	const tier = await getSubscriptionTier(session.user.id);
+
+	if (tier !== "Free") {
+		throw redirect(302, "/account/billing");
 	}
 
 	const priceId = event.url.searchParams.get("id");
